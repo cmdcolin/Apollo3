c46eb33d9d8c92dd9472aa000bc4d0b3afd0a49e
Colin <colin.diesh@gmail.com>
2026-03-09 09:12:57 -0400

    Update cypress

--- packages/jbrowse-plugin-apollo/cypress/support/e2e.ts
+++ packages/jbrowse-plugin-apollo/cypress/support/e2e.ts
@@ -13,40 +13,11 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// For Cypress v12.17.3 and older:
 import compareSnapshotCommand from 'cypress-image-diff-js'
 // Import commands.js using ES2015 syntax:
 import './commands'
 
 compareSnapshotCommand()
-
-// Polyfill ES2023 array methods for Electron 106 (Cypress 12.17.3 uses
-// Chrome 106 which predates these methods, but JBrowse v4 uses them)
-Cypress.on('window:before:load', (win) => {
-  const proto = win.Array.prototype
-  if (!proto.toSorted) {
-    proto.toSorted = function (compareFn?: (a: never, b: never) => number) {
-      return [...this].sort(compareFn)
-    }
-  }
-  if (!proto.toReversed) {
-    proto.toReversed = function () {
-      return [...this].reverse()
-    }
-  }
-  if (!proto.findLast) {
-    proto.findLast = function (
-      fn: (v: never, i: number, a: never[]) => boolean,
-    ) {
-      for (let i = this.length - 1; i >= 0; i--) {
-        if (fn(this[i], i, this)) {
-          return this[i]
-        }
-      }
-      return undefined
-    }
-  }
-})
 
 // Collect browser console errors and debug warnings for later flushing.
 // Cannot call cy.task() directly inside window:before:load as it causes
